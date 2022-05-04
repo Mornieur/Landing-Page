085e2857b950927fe39298a5985bd8015e8ae355
Nanda <mariafernanda-coro@hotmail.com>
2022-05-03 22:11:34 -0300

    Adicionando modal de cadastro

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
+
 import "./scss/main.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
